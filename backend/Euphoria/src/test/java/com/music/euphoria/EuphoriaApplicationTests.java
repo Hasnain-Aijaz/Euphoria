624a2168f62a7213dc9dced1e68479364607db74
Hasnain <hasnainaijaz123@gmail.com>
2026-05-14 16:40:36 +0500

    Initial Commit

---
+++ backend/Euphoria/src/test/java/com/music/euphoria/EuphoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.euphoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuphoriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
